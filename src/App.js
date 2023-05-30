1f4b2d1dd70f5d44b0cf39e38670a4e9f173b7c9
Stiven Gordillo Ruiz <stiven@stivengordillo.com>
2023-05-29 20:24:12 -0500

    inicio aprendizaje

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { TodoCounter }      from "./components/TodoCounter";
+import { TodoSearch }       from "./components/TodoSearch";
+import { TodoList }         from "./components/TodoList";
+import { TodoItem }         from "./components/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton";
+import React
+ from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter completed={1} total={5} />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
+
 
 export default App;
